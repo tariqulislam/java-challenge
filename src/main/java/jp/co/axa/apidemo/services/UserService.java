014495ff445bfb75cdc821cf849f8831d8db893c
Tariqul Islam <mdtariqul.islam@rakuten.com>
2023-09-12 22:34:28 +0900

    Documenting the service related to user information

--- src/main/java/jp/co/axa/apidemo/services/UserService.java
+++ src/main/java/jp/co/axa/apidemo/services/UserService.java
@@ -7,15 +7,22 @@ import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+/* User Service implementation
+* Handling the logic for login and register user*/
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    /* Service Method for find the user by email
+    * Using for validate the user*/
     public boolean findUserByEmail(String email) {
         User user = userRepository.findByEmail(email).orElse(null);
         return user != null;
     }
+
+    /* Service method for save user.
+    * user for register the user in application */
     public boolean saveUser(User user) {
         final String encryptedPassword = new BCryptPasswordEncoder().encode(user.getPassword());
         user.setPassword(encryptedPassword);
