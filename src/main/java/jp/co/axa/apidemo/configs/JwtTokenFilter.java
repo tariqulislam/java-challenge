79a1d802dfbea655b17c134aa512e6ba7b9f5137
Tariqul Islam <mdtariqul.islam@rakuten.com>
2023-09-12 22:01:28 +0900

    Documenting the JwtToken Handling class

--- src/main/java/jp/co/axa/apidemo/configs/JwtTokenFilter.java
+++ src/main/java/jp/co/axa/apidemo/configs/JwtTokenFilter.java
@@ -2,8 +2,7 @@ package jp.co.axa.apidemo.configs;
 
 import jp.co.axa.apidemo.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,11 +17,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/* Jwt Token Handling class
+* Use: Once Per Request Filter.
+* Filter the unwanted token
+* Generate the token from user Details
+* */
 @Component
 public class JwtTokenFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtTokenConfig jwtTokenConfig;
+    /* Method for how the token will be filters  for Http Request
+    *  Override Authentication context json token and filter the authentication during Request and Response Handling */
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
         if(!hasAuthorizationBearer(httpServletRequest)) {
@@ -41,17 +47,25 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 
+    /* Function for check the Token format
+    *  Request Header Authorization has start with Bearer  */
     private boolean hasAuthorizationBearer(HttpServletRequest request) {
         String header = request.getHeader("Authorization");
         return !ObjectUtils.isEmpty(header) && header.startsWith("Bearer");
     }
 
 
+    /* Function for check the Token format
+     *  split the access token from request header pass into different
+     * function for filter token and authentication context   */
     private String getAccessToken(HttpServletRequest request) {
         String header = request.getHeader("Authorization");
         return header.split(" ")[1].trim();
     }
 
+    /* Set the Authentication context using UserDetails from Spring Core
+    * Define the Authentication Behavior
+    * Built the context to using during token authentication */
     private void setAuthenticationContext(String token, HttpServletRequest request) {
         UserDetails userDetails = getUserDetails(token);
         UsernamePasswordAuthenticationToken authenticationToken =
@@ -60,6 +74,8 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
     }
 
+    /* Get the Access token form http request and extract user information
+    *  Assign the Spring Core UserDetails for Token Based Authentication*/
     private UserDetails getUserDetails(String token) {
         User userDetails = new User();
         String[] jwtSubject = jwtTokenConfig.getSubject(token).split(",");
