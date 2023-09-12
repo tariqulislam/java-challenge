7599d9be695416a25811c51f1dd033cc8acd94c3
Tariqul Islam <mdtariqul.islam@rakuten.com>
2023-09-12 22:57:29 +0900

    add the documentation to user controller

--- src/main/java/jp/co/axa/apidemo/controllers/UserController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
-
+/* Rest api controller for authentication and access token */
 @RestController
 @RequestMapping(path = "/auth")
 public class UserController {
@@ -27,13 +27,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /* Login method for get the access token from server */
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid AuthRequest request) {
         try {
+            /* Authentication with spring authentication manager using username and password token authentication types */
             Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
 
             User user = (User) authentication.getPrincipal();
             String accessToken = jwtTokenConfig.generateAccessToken(user);
+            /* Create the response  using Auth Response  class */
             AuthResponse response = new AuthResponse(user.getEmail(), accessToken);
             return ResponseEntity.ok().body(response);
         }catch (BadCredentialsException ex) {
@@ -44,9 +47,13 @@ public class UserController {
         }
     }
 
+    /* Rest api for user Registration to database */
     @PostMapping("/register")
     public  ResponseEntity<?> register(@RequestBody @Valid AuthRequest request) {
+        /* Create the Hash map to handle the response
+        * And serialize response as json format  */
         HashMap<String, String> map = new HashMap<>();
+        /* Check the Same user already into database */
         if(userService.findUserByEmail(request.getEmail())) {
             map.put("message", "User Already Exists");
             map.put("email", request.getEmail());
@@ -54,6 +61,7 @@ public class UserController {
             return ResponseEntity.ok().body(map);
         } else {
             try {
+                /* Register the user */
                 User saveUserInfo = new User();
                 saveUserInfo.setEmail(request.getEmail());
                 saveUserInfo.setPassword(request.getPassword());
